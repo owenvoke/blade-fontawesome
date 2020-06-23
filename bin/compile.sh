465f80179000a0c724cc075f7eaf763b3716edaa
Owen Voke <development@voke.dev>
2020-06-23 11:05:45 +0100

    Add script to compile icons

---
+++ bin/compile.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIRECTORY=$(cd "$(dirname "$0")" && pwd)
+ICONS_ROOT=$DIRECTORY/../dist
+RESOURCES_ROOT=$DIRECTORY/../resources/svg
+STYLES=(brands regular solid)
+
+echo "Compiling Font Awesome..."
+
+for STYLE in "${STYLES[@]}"; do
+    ICONS_DIR="${ICONS_ROOT}/${STYLE}"
+    RESOURCES_DIR="${RESOURCES_ROOT}/${STYLE}"
+
+    if [ ! -d "${RESOURCES_DIR}" ]; then
+        mkdir "${RESOURCES_DIR}"
+    fi
+
+    for FILE in "${ICONS_DIR}"/*; do
+      FILENAME=${FILE##*/}
+
+      if [ "$FILENAME" == ".gitignore" ]
+      then
+        break
+      fi
+
+      # Compile icons...
+      cp "${FILE}" "${RESOURCES_DIR}/${FILENAME}"
+
+      CLASS='<svg fill="currentColor"'
+      sed -i '' "s/<svg/${CLASS}/" "${RESOURCES_DIR}/${FILENAME}"
+    done
+done
+
+echo "All done!"
