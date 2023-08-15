b1bc68611291890448aa713225e17971859f3f57
Owen Voke <development@voke.dev>
2023-08-15 11:18:38 +0100

    chore: update to use Pest built-in snapshots

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -3,24 +3,23 @@
 declare(strict_types=1);
 
 use OwenVoke\BladeFontAwesome\Tests\TestCase;
-use function Spatie\Snapshots\assertMatchesSnapshot;
 
 uses(TestCase::class);
 
 it('compiles a single anonymous component', function () {
     $result = svg('fas-plus')->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
 
 it('can add classes to icons', function () {
     $result = svg('fas-plus', 'w-6 h-6 text-gray-500')->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
 
 it('can add styles to icons', function () {
     $result = svg('fas-plus', ['style' => 'color: #555'])->toHtml();
 
-    assertMatchesSnapshot($result);
+    expect($result)->toMatchSnapshot();
 });
